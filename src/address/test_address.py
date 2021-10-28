2ba59a06f9cba22fbdfc2772feb70fdadcfe6d0e
Alisamar Husain <zrthxn@gmail.com>
2021-10-29 00:23:52 +0530

    Print variable in tests (#2)

--- src/address/test_address.py
+++ src/address/test_address.py
@@ -14,22 +14,30 @@ b address.cpp:9
 b address.cpp:14
 r
 script lldb.debugger.HandleCommand("frame var ip4")
+script lldb.debugger.HandleCommand("p ip4")
 c
 script lldb.debugger.HandleCommand("frame var ip4")
+script lldb.debugger.HandleCommand("p ip4")
 c
 script lldb.debugger.HandleCommand("frame var ip6")
+script lldb.debugger.HandleCommand("p ip6")
 c
 script lldb.debugger.HandleCommand("frame var ip6")
+script lldb.debugger.HandleCommand("p ip6")
 c
 """
         expected = """
 (folly::IPAddress) ip4 = (folly::IPAddress) 10.0.0.10
+(folly::IPAddress) $0 = (folly::IPAddress) 10.0.0.10
 10.0.0.10
 (folly::IPAddress) ip4 = (folly::IPAddress) 192.168.0.1
+(folly::IPAddress) $1 = (folly::IPAddress) 192.168.0.1
 192.168.0.1
 (folly::IPAddress) ip6 = (folly::IPAddress) 0000:0000:0000:0000:0000:0000:0000:0001
+(folly::IPAddress) $2 = (folly::IPAddress) 0000:0000:0000:0000:0000:0000:0000:0001
 ::1
 (folly::IPAddress) ip6 = (folly::IPAddress) 2620:0000:1cfe:face:b00c:0000:0000:0003
+(folly::IPAddress) $3 = (folly::IPAddress) 2620:0000:1cfe:face:b00c:0000:0000:0003
 2620:0:1cfe:face:b00c::3
 """.strip()
 
