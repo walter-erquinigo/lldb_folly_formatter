a50a3f182b9fd2f7d5acf52958251d09a1731161
Alisamar Husain <zrthxn@gmail.com>
2021-10-30 00:01:18 +0530

    Added test for Socket Address

--- src/address/address.cpp
+++ src/address/address.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <folly/IPAddress.h>
+#include <folly/MacAddress.h>
+#include <folly/SocketAddress.h>
 
 using namespace folly;
 
@@ -14,12 +16,24 @@ void printIP6(std::string ip6_str) {
   std::cout << ip6 << std::endl;
 }
 
+void printSocket(std::string host_str, uint16_t port) {
+  SocketAddress sock(host_str, port);
+  std::cout << sock << std::endl;
+}
+
 int main() {
+  // IPv4 addresses
   printIP4("10.0.0.10");
   printIP4("192.168.0.1");
 
+  // IPv6 Addresses
   printIP6("::1");
   printIP6("2620:0:1cfe:face:b00c::3");
+
+  printSocket("10.0.0.10", 3000);
+  printSocket("192.168.0.1", 4000);
+  printSocket("::1", 5000);
+  printSocket("2620:0:1cfe:face:b00c::3", 6000);
 
   return 0;
 }
--- src/address/test_address.py
+++ src/address/test_address.py
@@ -10,8 +10,9 @@ class FollyAddress(LLDBTestCase):
 
     def test_address(self):
         script = """
-b address.cpp:9
-b address.cpp:14
+b address.cpp:11
+b address.cpp:16
+b address.cpp:21
 r
 script lldb.debugger.HandleCommand("frame var ip4")
 script lldb.debugger.HandleCommand("p ip4")
@@ -25,7 +26,20 @@ c
 script lldb.debugger.HandleCommand("frame var ip6")
 script lldb.debugger.HandleCommand("p ip6")
 c
+script lldb.debugger.HandleCommand("frame var sock")
+script lldb.debugger.HandleCommand("p sock")
+c
+script lldb.debugger.HandleCommand("frame var sock")
+script lldb.debugger.HandleCommand("p sock")
+c
+script lldb.debugger.HandleCommand("frame var sock")
+script lldb.debugger.HandleCommand("p sock")
+c
+script lldb.debugger.HandleCommand("frame var sock")
+script lldb.debugger.HandleCommand("p sock")
+c
 """
+
         expected = """
 (folly::IPAddress) ip4 = (folly::IPAddress) 10.0.0.10
 (folly::IPAddress) $0 = (folly::IPAddress) 10.0.0.10
@@ -39,6 +53,18 @@ c
 (folly::IPAddress) ip6 = (folly::IPAddress) 2620:0000:1cfe:face:b00c:0000:0000:0003
 (folly::IPAddress) $3 = (folly::IPAddress) 2620:0000:1cfe:face:b00c:0000:0000:0003
 2620:0:1cfe:face:b00c::3
+(folly::SocketAddress) sock = (folly::SocketAddress) 10.0.0.10:3000
+(folly::SocketAddress) $4 = (folly::SocketAddress) 10.0.0.10:3000
+10.0.0.10:3000
+(folly::SocketAddress) sock = (folly::SocketAddress) 192.168.0.1:4000
+(folly::SocketAddress) $5 = (folly::SocketAddress) 192.168.0.1:4000
+192.168.0.1:4000
+(folly::SocketAddress) sock = (folly::SocketAddress) [0000:0000:0000:0000:0000:0000:0000:0001]:5000
+(folly::SocketAddress) $6 = (folly::SocketAddress) [0000:0000:0000:0000:0000:0000:0000:0001]:5000
+[::1]:5000
+(folly::SocketAddress) sock = (folly::SocketAddress) [2620:0000:1cfe:face:b00c:0000:0000:0003]:6000
+(folly::SocketAddress) $7 = (folly::SocketAddress) [2620:0000:1cfe:face:b00c:0000:0000:0003]:6000
+[2620:0:1cfe:face:b00c::3]:6000
 """.strip()
 
         self.assertEquals(expected, self.run_lldb("address", script))
