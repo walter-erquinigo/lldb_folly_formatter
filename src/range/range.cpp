4a6d9674a144a6b5cf8d069c2974e83ee8ce21b4
Alisamar Husain <zrthxn@gmail.com>
2021-11-03 00:35:10 +0530

    Added new tests to Range
    Added new tests to Range

--- src/range/range.cpp
+++ src/range/range.cpp
@@ -8,22 +8,46 @@ void printStringPiece(std::string _piece) {
   std::cout << str << std::endl;
 }
 
-// void printIntArray(int arr[], int size) {
-//   int array[size] = arr;
-//   std::cout << str << std::endl;
-// }
+template <typename T>
+Range<const T*> RangeFromVector(std::vector<T> vec) {
+  auto range = Range<const T*>(&vec[0], vec.size());
+  return range;
+}
+
+ByteRange ByteRangeFromStringPiece(std::string _piece) {
+  StringPiece str(_piece);
+  ByteRange byter(str);
+  return byter;
+}
+
+MutableByteRange MutByteRangeFromStringPiece(std::string _piece) {
+  MutableStringPiece str(&_piece.front(), _piece.size());
+  MutableByteRange byter(str);
+  return byter;
+}
 
 int main() {
   // Test simple StringPieces
   printStringPiece("Hello");
   printStringPiece("Hello, World!");
-  printStringPiece("12345067891");
+  printStringPiece("1234567890");
   
-  // Test whether null char is handled
-  // Should only show "Test"
+  // Test whether null char is handled. Should only show "Test"
   printStringPiece("Test\0Null");
 
-  // What else?
+  // From vectors
+  RangeFromVector(std::vector<uint32_t>({ 1, 2, 3, 4 }));
+  RangeFromVector(std::vector<float_t>({ 1.0, 2.0, 3.0 }));
+  RangeFromVector(std::vector<char32_t>({ 'A', 'B' }));
+
+  // Simple ByteRange
+  ByteRangeFromStringPiece("Hello, World!");
+  // Mutable ByteRange
+  MutByteRangeFromStringPiece("Hello, World!");
+
+  // Lastly, empty array
+  std::array<int, 0> empty{};
+  constexpr auto EmptyRange = Range<const int*>{empty};
 
   return 0;
 }
--- src/range/test_range.py
+++ src/range/test_range.py
@@ -7,4 +7,72 @@ class FollyRangeTest(LLDBTestCase):
         return path.join(curr_dir, "range_formatter.py")
 
     def test_range(self):
-        pass
+        script = """
+b range.cpp:8
+b range.cpp:14
+b range.cpp:20
+b range.cpp:26
+b range.cpp:50
+r
+script lldb.debugger.HandleCommand("frame var str")
+script lldb.debugger.HandleCommand("p str")
+c
+script lldb.debugger.HandleCommand("frame var str")
+script lldb.debugger.HandleCommand("p str")
+c
+script lldb.debugger.HandleCommand("frame var str")
+script lldb.debugger.HandleCommand("p str")
+c
+script lldb.debugger.HandleCommand("frame var str")
+script lldb.debugger.HandleCommand("p str")
+c
+script lldb.debugger.HandleCommand("frame var range")
+script lldb.debugger.HandleCommand("p range")
+c
+script lldb.debugger.HandleCommand("frame var range")
+script lldb.debugger.HandleCommand("p range")
+c
+script lldb.debugger.HandleCommand("frame var range")
+script lldb.debugger.HandleCommand("p range")
+c
+script lldb.debugger.HandleCommand("frame var byter")
+script lldb.debugger.HandleCommand("p byter")
+c
+script lldb.debugger.HandleCommand("frame var byter")
+script lldb.debugger.HandleCommand("p byter")
+c
+n
+n
+script lldb.debugger.HandleCommand("frame var EmptyRange")
+script lldb.debugger.HandleCommand("p EmptyRange")
+c
+"""
+
+        expected = """
+(folly::StringPiece) str = (folly::StringPiece) "Hello"
+(folly::StringPiece) $0 = (folly::StringPiece) "Hello"
+Hello
+(folly::StringPiece) str = (folly::StringPiece) "Hello, World!"
+(folly::StringPiece) $1 = (folly::StringPiece) "Hello, World!"
+Hello, World!
+(folly::StringPiece) str = (folly::StringPiece) "1234567890"
+(folly::StringPiece) $2 = (folly::StringPiece) "1234567890"
+1234567890
+(folly::StringPiece) str = (folly::StringPiece) "Test"
+(folly::StringPiece) $3 = (folly::StringPiece) "Test"
+Test
+(folly::Range<const unsigned int *>) range = (folly::Range) <const unsigned int *> [4]
+(folly::Range<const unsigned int *>) $4 = (folly::Range) <const unsigned int *> [4]
+(folly::Range<const float *>) range = (folly::Range) <const float *> [3]
+(folly::Range<const float *>) $5 = (folly::Range) <const float *> [3]
+(folly::Range<const char32_t *>) range = (folly::Range) <const char32_t *> [2]
+(folly::Range<const char32_t *>) $6 = (folly::Range) <const char32_t *> [2]
+(folly::ByteRange) byter = (folly::Range) <const unsigned char *> [13]
+(folly::ByteRange) $7 = (folly::Range) <const unsigned char *> [13]
+(folly::MutableByteRange) byter = (folly::Range) <unsigned char *> [13]
+(folly::MutableByteRange) $8 = (folly::Range) <unsigned char *> [13]
+(const folly::Range<const int *>) EmptyRange = (folly::Range) <const int *> [0]
+(const folly::Range<const int *>) $9 = (folly::Range) <const int *> [0]
+""".strip()
+
+        self.assertEqual(expected, self.run_lldb("range", script))
