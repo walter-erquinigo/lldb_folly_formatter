e59776f332cfcc15a3cf3964ed0f92c737ccc7cc
Alisamar Husain <zrthxn@gmail.com>
2021-11-13 00:03:29 +0530

    Script for new Formatter

---
+++ new.sh
@@ -0,0 +1,15 @@
+classname=$1
+
+mkdir -p src/$classname
+cd src/$classname
+touch __init__.py ${classname}_formatter.py test_${classname}.py
+touch ${classname}.cpp
+
+cat << EOF > CMakeLists.txt
+add_executable(${classname} ${classname}.cpp)
+
+target_link_libraries(${classname} PRIVATE folly)
+
+install(TARGETS ${classname}
+	RUNTIME DESTINATION ${INSTALL_DIR})
+EOF
