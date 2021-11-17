f249b2b4a546e5d38806438bdb4b3dee9ce1a117
Alisamar Husain <zrthxn@gmail.com>
2021-11-18 01:17:06 +0530

    Fix FBString test (#9)

--- src/fbstring/test_fbstring.py
+++ src/fbstring/test_fbstring.py
@@ -40,25 +40,25 @@ c
 """
         expected = """
 (folly::fbstring) str = (folly::fbstring) ""
-(folly::fbstring) $0 = (folly::fbstring) ""
+(folly::fbstring) $1 = (folly::fbstring) ""
 
 (folly::fbstring) str = (folly::fbstring) "a"
-(folly::fbstring) $1 = (folly::fbstring) "a"
+(folly::fbstring) $4 = (folly::fbstring) "a"
 a
 (folly::fbstring) str = (folly::fbstring) "aa"
-(folly::fbstring) $2 = (folly::fbstring) "aa"
+(folly::fbstring) $7 = (folly::fbstring) "aa"
 aa
 (folly::fbstring) str = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaa"
-(folly::fbstring) $3 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaa"
+(folly::fbstring) $10 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaa"
 aaaaaaaaaaaaaaaaaaaaaaa
 (folly::fbstring) str = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaa"
-(folly::fbstring) $4 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaa"
+(folly::fbstring) $12 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaa"
 aaaaaaaaaaaaaaaaaaaaaaaa
 (folly::fbstring) str = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaaa"
-(folly::fbstring) $5 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaaa"
+(folly::fbstring) $13 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaaa"
 aaaaaaaaaaaaaaaaaaaaaaaaa
 (folly::fbstring) str = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-(folly::fbstring) $6 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+(folly::fbstring) $14 = (folly::fbstring) "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 """.strip()
 
