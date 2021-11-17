b064baddbea9d45dac0f2feb1cf5194bae0b14f5
Alisamar Husain <zrthxn@gmail.com>
2021-11-18 01:23:26 +0530

    Small change to new script (#10)
    Small change to new script

--- new.sh
+++ new.sh
@@ -3,7 +3,14 @@ classname=$1
 mkdir -p src/$classname
 cd src/$classname
 touch __init__.py ${classname}_formatter.py test_${classname}.py
-touch ${classname}.cpp
+
+cat << EOF > ${classname}.cpp
+#include <iostream>
+
+int main() {
+  return 0;
+}
+EOF
 
 cat << EOF > CMakeLists.txt
 add_executable(${classname} ${classname}.cpp)
@@ -11,7 +18,7 @@ add_executable(${classname} ${classname}.cpp)
 target_link_libraries(${classname} PRIVATE folly)
 
 install(TARGETS ${classname}
-	RUNTIME DESTINATION ${INSTALL_DIR})
+	RUNTIME DESTINATION \${INSTALL_DIR})
 EOF
 
 cd ..
