6fad523229827c147d64e7ae8d63a1ea30b70b57
Alisamar Husain <zrthxn@gmail.com>
2021-11-23 03:44:58 +0530

    Temporary fix for broken Optional test

--- src/optional/test_optional.py
+++ src/optional/test_optional.py
@@ -28,14 +28,22 @@ c
 """
 
         expected = """
-(folly::Optional<int>) someInt = No value
-(folly::Optional<int>) $0 = No value
-(folly::Optional<int>) someInt = (int) value = 34
-(folly::Optional<int>) $1 = (int) value = 34
-(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) someFbStr = No value
-(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) $2 = No value
-(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) someFbStr = (folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> >) value = (store_ = (folly::fbstring) "Hello, World!")
-(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) $4 = (folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> >) value = (store_ = (folly::fbstring) "Hello, World!")
+(folly::Optional<int>) someInt = Has Value=false {}
+(folly::Optional<int>) $0 = Has Value=false {}
+(folly::Optional<int>) someInt = Has Value=true {
+value = 34
+}
+(folly::Optional<int>) $1 = Has Value=true {
+value = 34
+}
+(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) someFbStr = Has Value=false {}
+(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) $2 = Has Value=false {}
+(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) someFbStr = Has Value=true {
+value = (store_ = (folly::fbstring) "Hello, World!")
+}
+(folly::Optional<folly::basic_fbstring<char, std::char_traits<char>, std::allocator<>, folly::fbstring_core<char> > >) $4 = Has Value=true {
+value = (store_ = (folly::fbstring) "Hello, World!")
+}
 """.strip()
 
         self.assertEqual(expected, self.run_lldb("optional", script))
