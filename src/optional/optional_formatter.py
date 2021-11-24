4f7e42ddd3ec560cc4f37725ccbd128c8246da38
Alisamar Husain <zrthxn@gmail.com>
2021-11-25 01:12:10 +0530

    Summary improvement

--- src/optional/optional_formatter.py
+++ src/optional/optional_formatter.py
@@ -40,6 +40,11 @@ class OptionalFormatter:
 		return True
 
 
+def OptionalSummary(valobj, dict):
+	hasValue = valobj.GetNumChildren() > 0
+	return f"Has Value={'true' if hasValue else 'false'}"
+
+
 def __lldb_init_module(debugger, dict):
     typeName = r"(^folly::Optional<.*$)"
     moduleName = os.path.splitext(os.path.basename(__file__))[0]
@@ -51,7 +56,7 @@ def __lldb_init_module(debugger, dict):
     )
 
     debugger.HandleCommand(
-        'type summary add --expand ' 
+        'type summary add --expand --hide-empty --no-value ' 
         + f'-x "{typeName}" ' 
-        + f'--summary-string "Has Value=${{var.storage_.hasValue}}"'
+        + f'--python-function {moduleName}.OptionalSummary'
     )
